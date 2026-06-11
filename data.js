14124abb5016e6170bcfe5c7a4bc7325d07b3da3
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-06-11 23:00:40 +0630

    Link js file in html and built an array object for menu data in data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+const menu = [
+    {foodName : 'Pizza' ,
+     ingredients: 'pepperoni,mushrom,mozarella',
+     price: 14 ,
+     image:'🍕'
+    } ,
+    {foodName : 'Hamburger' ,
+     ingredients: 'beef, cheese, lettuce',
+     price: 12 ,
+     image:'🍔'
+    } ,
+    {foodName : 'Beer' ,
+     ingredients: 'grain, hops, yeast, water',
+     price: 12 ,
+     image:'🍺'
+    } 
+]
