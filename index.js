a614e2d64bb683b2f21d49cd6da00a58969e3518
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-06-12 03:12:58 +0630

    style and give size to food image , subheading , price and button

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 import { menu } from "./data.js";
 
 
-function render(){
-    return menu.map(posts=>
-        ` <div class="food-container">
-         <div class="food-image">
-         <p> ${posts.image}</p>
-         </div>
+function render() {
+    return menu.map(posts => `
+        <section class="food-card">
+            <div class="food-container">
 
-         <div class="price-text">
-         <p>${posts.foodName}</p>
-         <p>${posts.ingredients}</p>
-         <p>${posts.price}</p>
-         </div>
-         
-         <div id="addCart"> +</div>`
-      ).join('')
+                <div class="food-image">
+                    <p>${posts.image}</p>
+                </div>
+
+                <div class="price-text">
+                    <h2>${posts.foodName}</h2>
+                    <p>${posts.ingredients}</p>
+                    <p class="price">$${posts.price}</p>
+                </div>
+
+                <div class="addCart">+</div>
+
+            </div>
+        </section>
+    `).join('');
 }
 
 document.getElementById('container').innerHTML = render()
